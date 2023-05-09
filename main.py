48c0514d16e507c40c5840b5f16fa8934a4dded8
Alessandro Bertozzi <alessandro.bertozzi7@studio.unibo.it>
2023-05-09 19:22:49 +0200

    add log and divide the embeddings in batch of sentences

--- main.py
+++ main.py
@@ -6,18 +6,23 @@ import stanza
 import os
 from serica.write import exist_document
 import threading
+from logging.config import dictConfig
+import logging
+
+logging.config.fileConfig('logging.conf', disable_existing_loggers=False)
+logger = logging.getLogger(__name__)
 
 app = FastAPI()
 
 # Get environment variables
-DB_HOST = os.getenv("DB_HOST","localhost")
-DB_PORT = os.getenv("DB_PORT", "9201")
+DB_HOST = os.getenv("DB_HOST", "localhost")
+DB_PORT = os.getenv("DB_PORT", "9200")
 DB_USER = os.getenv("DB_USER", "")
 DB_PASS = os.getenv("DB_PASS", "")
 doc_index = os.getenv("doc_index", "sentences")
 # Database connection
 client = Elasticsearch(
-    "http://"+DB_HOST+":"+DB_PORT,
+    "http://" + DB_HOST + ":" + DB_PORT,
     basic_auth=(DB_USER, DB_PASS),
     verify_certs=True,
     request_timeout=10
@@ -32,6 +37,7 @@ nlp = stanza.Pipeline(lang="la", processors="tokenize")
 
 @app.get("/")
 async def root():
+    logger.info("Start parsing")
     return {"message": "This is the vector embeddings API for Serica!"}
 
 
@@ -45,35 +51,54 @@ async def vector(query: Query):
     return {"vector": model(query.query_params)[0, :].tolist()}
 
 
-
 def long_running_task(**kwargs):
     text = kwargs.get('text')
     title = kwargs.get('title')
     slug = kwargs.get('slug')
+    logger.info("Start parsing")
     document_id = kwargs.get('document_id')
 
     parsed = nlp(text)
+
+    logger.info("Parsed")
     sentences = [sentence.text for sentence in parsed.sentences]
-    embeddings = model(sentences)
 
-    actions = list()
+    number_index_documents = 0
+    while len(sentences) > 0:
 
-    for i, sentence in enumerate(zip(sentences, embeddings)):
-        doc = {
-            "_index": "sentences",
-            "_source": {
-                "title": title,
-                "slug": slug,
-                "document_id": document_id,
-                "number": i,
-                "sentence": sentence[0],
-                "LaBSE_features": sentence[1]
+        sentence_sample = sentences[:500]
+        del sentences[:500]
+
+        logger.info(len(sentences))
+
+        logger.info("Start creating embeddings")
+        embeddings = model(sentence_sample)
+
+        actions = list()
+
+        logger.info("Start creating vocab")
+        for i, sentence in enumerate(zip(sentence_sample, embeddings)):
+            doc = {
+                "_index": "sentences",
+                "_source": {
+                    "title": title,
+                    "slug": slug,
+                    "document_id": document_id,
+                    "number": i + number_index_documents,
+                    "sentence": sentence[0],
+                    "LaBSE_features": sentence[1]
+                }
             }
-        }
 
-        actions.append(doc)
+            actions.append(doc)
 
-    helpers.bulk(client, actions)
+        logger.info("Start indexing")
+        helpers.bulk(client, actions)
+
+        logger.info("End partial indexing")
+        number_index_documents += 500
+    logger.info("End indexing")
+
 
 class Transcription(BaseModel):
     title: str
@@ -81,9 +106,9 @@ class Transcription(BaseModel):
     slug: str
     text: str
 
+
 @app.put("/insertion/")
 async def insertion(transcription: Transcription):
-
     document_id = transcription.document_id
     title = transcription.title
     slug = transcription.slug
@@ -92,7 +117,6 @@ async def insertion(transcription: Transcription):
 
     if client.indices.exists(index=doc_index):
         if exist_document(client, doc_index, 'document_id', document_id):
-
             # Delete the document data for document_id
             client.delete_by_query(
                 index=doc_index, query={"term": {"document_id": document_id}}
@@ -106,29 +130,6 @@ async def insertion(transcription: Transcription):
                                                                     "document_id": document_id
                                                                     })
         thread.start()
-
-        # parsed = nlp(text)
-        # sentences = [sentence.text for sentence in parsed.sentences]
-        # embeddings = model(sentences)
-        #
-        # actions = list()
-        #
-        # for i, sentence in enumerate(zip(sentences, embeddings)):
-        #     doc = {
-        #             "_index": "sentences",
-        #             "_source": {
-        #                 "title": title,
-        #                 "slug": slug,
-        #                 "document_id": document_id,
-        #                 "number": i,
-        #                 "sentence": sentence[0],
-        #                 "LaBSE_features": sentence[1]
-        #             }
-        #         }
-        #
-        #     actions.append(doc)
-        #
-        # helpers.bulk(client, actions)
     else:
         output["status"] = 404
         output["message"] = "Index not found"
@@ -138,7 +139,6 @@ async def insertion(transcription: Transcription):
 
 @app.delete("/deletion/{document_id}/")
 async def deletion(document_id):
-
     output = {"status": 200, "message": "Document deleted"}
     if client.indices.exists(index=doc_index):
         if exist_document(client, doc_index, 'document_id', document_id):
@@ -154,7 +154,4 @@ async def deletion(document_id):
         output["status"] = 404
         output["message"] = "Index not found"
 
-
     return output
-
-
